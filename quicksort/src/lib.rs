bc02ec97abd12e9fd97047a9cc75f8d039ca46a4
C <DeveloperC@protonmail.com>
2020-04-18 00:47:09 +0000

    Converted i32 usage -> usize.
    Changed the usage of i32 to usize and removed unnesscary partition and
    and function calls.

--- quicksort/src/lib.rs
+++ quicksort/src/lib.rs
@@ -2,17 +2,27 @@
 extern crate log;
 
 pub fn quicksort(to_sort: Vec<i32>) -> Vec<i32> {
-    let ending_index = to_sort.len() as i32 - 1;
-    return quicksort_internal(to_sort, 0, ending_index);
+    if to_sort.len() > 2 {
+        let ending_index = to_sort.len() - 1;
+        return quicksort_internal(to_sort, 0, ending_index);
+    }
+
+    return to_sort;
 }
 
-fn quicksort_internal(mut to_sort: Vec<i32>, starting_index: i32, ending_index: i32) -> Vec<i32> {
+fn quicksort_internal(
+    mut to_sort: Vec<i32>,
+    starting_index: usize,
+    ending_index: usize,
+) -> Vec<i32> {
     if starting_index < ending_index {
         let (pivot_point_index, partitioned_to_sort) =
             sort_partition(to_sort, starting_index, ending_index);
         to_sort = partitioned_to_sort;
 
-        to_sort = quicksort_internal(to_sort, starting_index, pivot_point_index - 1);
+        if pivot_point_index > 0 {
+            to_sort = quicksort_internal(to_sort, starting_index, pivot_point_index - 1);
+        }
         to_sort = quicksort_internal(to_sort, pivot_point_index + 1, ending_index);
     }
 
@@ -21,29 +31,29 @@ fn quicksort_internal(mut to_sort: Vec<i32>, starting_index: i32, ending_index: 
 
 fn sort_partition(
     mut to_sort: Vec<i32>,
-    starting_index: i32,
-    ending_index: i32,
-) -> (i32, Vec<i32>) {
+    starting_index: usize,
+    ending_index: usize,
+) -> (usize, Vec<i32>) {
     trace!(
         "starting_index {}, ending_index {}.",
         starting_index,
         ending_index
     );
     debug!("Partitioning {:?}.", to_sort);
-    let pivot = to_sort[ending_index as usize];
-    let mut i = starting_index as i32 - 1;
+    let pivot = to_sort[ending_index];
+    let mut i = starting_index;
 
     for j in starting_index..ending_index {
-        if to_sort[j as usize] < pivot {
+        if to_sort[j] < pivot {
+            if i != j {
+                to_sort.swap(i, j);
+                trace!("[{}] & [{}] switched {:?}", i, j, to_sort);
+            }
             i += 1;
-
-            to_sort.swap(i as usize, j as usize);
-            trace!("[{}] & [{}] switched {:?}", i, j, to_sort);
         }
     }
 
-    i += 1;
-    to_sort.swap(i as usize, ending_index as usize);
+    to_sort.swap(i, ending_index);
 
     debug!("Finished partitioning {:?}.", to_sort);
     return (i, to_sort);
