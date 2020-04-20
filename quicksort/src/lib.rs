f21be849cadf257711ea9cb690e87da7fb3ee3a0
C <DeveloperC@protonmail.com>
2020-04-20 21:13:19 +0000

    Removing unnessacary lets and ifs
    Merging multiple ifs into one and removing unnessacary lets.

--- quicksort/src/lib.rs
+++ quicksort/src/lib.rs
@@ -17,17 +17,12 @@ fn quicksort_internal(
             sort_partition(to_sort, starting_index, ending_index);
         to_sort = temp_to_sort;
 
-        if pivot_point_index > 0 {
-            let pivot_point_index_end = pivot_point_index - 1;
-
-            if pivot_point_index_end as i32 - starting_index as i32 > 0 {
-                to_sort = quicksort_internal(to_sort, starting_index, pivot_point_index_end);
-            }
+        if (pivot_point_index as i32 - 1) - starting_index as i32 > 0 {
+            to_sort = quicksort_internal(to_sort, starting_index, pivot_point_index - 1);
         }
 
-        let pivot_point_index_start = pivot_point_index + 1;
-        if ending_index as i32 - pivot_point_index_start as i32 > 0 {
-            to_sort = quicksort_internal(to_sort, pivot_point_index_start, ending_index);
+        if ending_index as i32 - (pivot_point_index as i32 + 1) > 0 {
+            to_sort = quicksort_internal(to_sort, pivot_point_index + 1, ending_index);
         }
     }
 
